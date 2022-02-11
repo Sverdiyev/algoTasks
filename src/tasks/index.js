431748523cb915572c445dc261c8c644c896fff0
Sasha Verdiyev <sasha.verdiyev@gmail.com>
2022-02-11 16:53:04 +0100

    setup & folder structure

---
+++ src/tasks/index.js
@@ -0,0 +1,6 @@
+// import { task1 } from './balancedBrackets.js';
+
+// const container1 = document.getElementById('container-1');
+// const container2 = document.getElementById('container-2');
+// const container3 = document.getElementById('container-3');
+// task1();
---
+++ src/tasks/task1.js
@@ -0,0 +1,4 @@
+export function task1() {
+	console.log('wow');
+	alert('connected task1');
+}
